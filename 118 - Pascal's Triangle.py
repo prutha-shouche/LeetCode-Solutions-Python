e32c134be7966992a9670860a21c7509575aadc8
prutha-shouche <106730796+prutha-shouche@users.noreply.github.com>
2022-06-17 22:42:21 +0300

    Add files via upload

---
+++ 118 - Pascal's Triangle.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 03:06:14 2022
+
+@author: pruth
+"""
+#Leetcode 118. Pascal's Triangle
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res=[[1]]
+        
+        for i in range(numRows-1):
+            temp=[0]+res[-1]+[0]
+            row=[]
+            for j in range(len(res[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            res.append(row)
+        return res
+                
+                
+        
+            
+                    
+        
+    
+        
+        
+
+            
+        
+        
+        
+        
+        
