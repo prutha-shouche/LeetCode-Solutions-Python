b4a0cc467b2e8d6016b648a6fb8fe2504a34056b
Prutha Shouche <106730796+prutha-shouche@users.noreply.github.com>
2022-08-18 20:15:25 -0700

    Update 1309 - Decrypt String from Alphabet to Integer Mapping.py

--- 1309 - Decrypt String from Alphabet to Integer Mapping.py
+++ 1309 - Decrypt String from Alphabet to Integer Mapping.py
@@ -9,10 +9,10 @@ Created on Sat Jun  4 03:06:14 2022
 class Solution:
     def freqAlphabets(self, s: str) -> str:
         d={'1':'a','2':'b','3':'c','4':'d','5':'e','6':'f','7':'g','8':'h','9':'i','10#':'j','11#':'k','12#':'l','13#':'m','14#':'n','15#':'o','16#':'p','17#':'q','18#':'r','19#':'s','20#':'t','21#':'u','22#':'v','23#':'w','24#':'x','25#':'y','26#':'z'}
-        if len(s)==1:
-            return d[s[0]]
-        if len(s)==2:
-            return d[s[0]]+d[s[1]]
+#         if len(s)==1:
+#             return d[s[0]]
+#         if len(s)==2:
+#             return d[s[0]]+d[s[1]]
         i=0
         s1=""
         while(i<len(s)-2):
