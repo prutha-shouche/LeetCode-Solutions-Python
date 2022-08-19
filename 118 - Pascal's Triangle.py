ee4edeed0f06b5b83ace22f7589fdfff92c47640
Prutha Shouche <106730796+prutha-shouche@users.noreply.github.com>
2022-08-18 19:24:12 -0700

    Update 118 - Pascal's Triangle.py

--- 118 - Pascal's Triangle.py
+++ 118 - Pascal's Triangle.py
@@ -10,10 +10,10 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res=[[1]]
         
-        for i in range(numRows-1):
-            temp=[0]+res[-1]+[0]
+        for i in range(numRows-1): 
+            temp=[0]+res[-1]+[0] #adding 0 to start and end of prev row
             row=[]
-            for j in range(len(res[-1])+1):
+            for j in range(len(res[-1])+1): #length of the prvious row (len(res[-1])+ 1
                 row.append(temp[j]+temp[j+1])
             res.append(row)
         return res
