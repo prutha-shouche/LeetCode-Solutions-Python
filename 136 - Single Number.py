7ac4303f66ef8bc1b10edb2cea08ce3f95c5c8a8
Prutha Shouche <106730796+prutha-shouche@users.noreply.github.com>
2022-08-20 17:03:05 -0700

    Update 136 - Single Number.py

--- 136 - Single Number.py
+++ 136 - Single Number.py
@@ -8,6 +8,15 @@ Created on Sat Jun  4 03:06:14 2022
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        #approach 1
+        arr= collections.Counter(nums) #A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+        for i in arr:
+            if arr[i]==1:
+                return i
+        
+        
+        
+        #approach 2
         #set is a data structure that only stores one occurence of the number
         #2*(a+b+c)-(2a+2b+c)=c
         return 2*sum(set(nums))-sum(nums)
